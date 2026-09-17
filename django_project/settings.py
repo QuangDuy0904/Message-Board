34008c2caf81b667ca26a0e76a108c0d57699af3
Quang Duy <duy1233d@gmail.com>
2026-09-17 10:21:21 +0700

    initial commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Email
